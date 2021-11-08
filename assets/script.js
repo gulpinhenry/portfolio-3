5f6dcdb058ac7881927a516e59284dca8b602c9f
Henry Kam <h3nrykam@gmail.com>
2021-11-07 23:26:00 -0800

    add icon and content

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 const collapseBtn = document.getElementById("collapse-btn");
 const mobileMenu = document.getElementById("offcanvas-nav");
-
+const test = document.getElementById("offcanvas-sd");
 
 collapseBtn.addEventListener("click", (event)=>{
     UIkit.offcanvas(mobileMenu).show();
